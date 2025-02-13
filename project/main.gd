f7364429b68839d6fa5fa7627f7d5e73adab8337
glaslan <glasd6702@gmail.com>
2025-02-13 12:36:26 -0500

    add squishy sound

--- project/main.gd
+++ project/main.gd
@@ -1,1 +1,9 @@
 extends Node
+
+func _ready() -> void:
+	var audio = AudioStreamPlayer3D.new()
+	audio.stream = preload("res://squish.wav")
+	audio.finished.connect(audio.play)
+	audio.volume_db = -5
+	add_child(audio)
+	audio.play()
