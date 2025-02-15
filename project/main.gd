c443b7001f21f298a33f3e65c4373c1ac2c1af47
glaslan <glasd6702@gmail.com>
2025-02-15 11:11:37 -0500

    add audio toggle button

--- project/main.gd
+++ project/main.gd
@@ -1,12 +1,18 @@
 extends Node
 
 var stream : AudioStream = preload("res://squish.wav")
+var audio : AudioStreamPlayer
 
 func _ready() -> void:
-	var audio = AudioStreamPlayer.new()
+	audio = AudioStreamPlayer.new()
 	audio.stream = stream
 	audio.finished.connect(audio.play) # Manually loops the audio
 	audio.volume_db = -5
 	add_child(audio)
-	if audio.playing == false:
+
+
+func _on_check_button_toggled(toggled_on: bool) -> void:
+	if toggled_on:
 		audio.play()
+	else:
+		audio.stop()
