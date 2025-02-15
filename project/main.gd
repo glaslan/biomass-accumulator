1d91bad13b458a4f1373c1dfb0fa1b0567d70d5a
glaslan <glasd6702@gmail.com>
2025-02-15 10:37:35 -0500

    Fixes #2 modify audio player

--- project/main.gd
+++ project/main.gd
@@ -1,8 +1,10 @@
 extends Node
 
+@export var stream : AudioStream
+
 func _ready() -> void:
 	var audio = AudioStreamPlayer3D.new()
-	audio.stream = preload("res://squish.wav")
+	audio.stream = stream
 	audio.finished.connect(audio.play)
 	audio.volume_db = -5
 	add_child(audio)
